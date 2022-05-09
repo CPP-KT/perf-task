368f2fbea705ad5be35d5465af2188465f59814e
lejabque <vorkdenis1@gmail.com>
2022-05-09 10:06:01 +0300

    format

--- ci-extra/perf/bench_pi.cpp
+++ ci-extra/perf/bench_pi.cpp
@@ -7,25 +7,24 @@
 // Computes PI using monte carlo method.
 // DO NOT change the algorithm.
 void BM_ComputePi(benchmark::State& state) {
-    int64_t total = 0;
-    int64_t inside_circle = 0;
+  int64_t total = 0;
+  int64_t inside_circle = 0;
 
-    srand(42);
+  srand(42);
 
-    for (auto _ : state) {
-        int x = rand() % 1000;
-        int y = rand() % 1000;
+  for (auto _ : state) {
+    int x = rand() % 1000;
+    int y = rand() % 1000;
 
-        total++;
-        if (x * x + y * y < 1000 * 1000) {
-            inside_circle++;
-        }
+    total++;
+    if (x * x + y * y < 1000 * 1000) {
+      inside_circle++;
     }
+  }
 
-    state.counters["pi"] = 4 * double(inside_circle) / total;
+  state.counters["pi"] = 4 * double(inside_circle) / total;
 }
 
-BENCHMARK(BM_ComputePi)
-    ->Threads(2);
+BENCHMARK(BM_ComputePi)->Threads(2);
 
 BENCHMARK_MAIN();
