3737ffac43936f0854328e07c5626225f37f1cee
Denis Vorkozhokov <30625073+lejabque@users.noreply.github.com>
2022-05-15 14:22:33 +0300

    Update bench_logger.cpp

--- perf/bench_logger.cpp
+++ perf/bench_logger.cpp
@@ -7,7 +7,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-// try to optimize without removing C-style file writing!
+// try to optimize without removing syscalls!
 class Logger {
 public:
   Logger(const std::string& path) {
